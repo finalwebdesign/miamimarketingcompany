50bc1dbd886fb9558c13e5919e41418575817c1c
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-09-04 17:02:26 -0500

    Image Assets

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,8 @@
   <div class="header">
       <img alt="Miami Marketing Company Logo" src="../assets/logo.png" />
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >
-    </p>
+    <p>What sets us apart is our interest in understanding your business and our ability to better connect your business strategy to our design. Miami Marketing Company is a leading provider of digital marketing and SEO services in the South Florida area.</p>
+    <p>Are you and your business ready to work with the leading digital marketing and search engine optimization firm in the Miami, FL area? Our team has years of experience marketing right here in the Miami Florida area. Visit our contact form here or call us direct at (305) 812-5999!</p>
   </div>
 </template>
 
@@ -27,7 +22,5 @@ export default class Header extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-p {
-  color: #42b983;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
-    <Header msg="Welcome to Your Vue.js + TypeScript App" />
-    <h1>READY TO WORK WITH MIAMI'S BEST?</h1>
+    <Header msg="The Best Marketing Firm in the Miami, FL area" />
+    <img src="../assets/miami-marketing-company.jpg" title="Miami Marketing Company" alt="Miami Marketing Company">
+<h1>READY TO WORK WITH MIAMI'S BEST?</h1>
     <p>Miami Marketing Company is here to help you and your online business! See more of our services below!</p>
-    <h2>Miami Marketing Company- Miamis Leading Website & Marketing Firm</h2>
+    <h2>Miami Marketing Company: Miami's Leading Website and Marketing Firm</h2>
     <p>Powerful Miami Website Deisgn and Development Serices. Miami's Leading Digital Marketing Firm.</p>
   </div>
 </template>
@@ -19,3 +20,9 @@ import Header from "@/components/Header.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
