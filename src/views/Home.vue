bd9ac1a8ee1b16d0bfb0349934874415fe7b59bc
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-09-04 11:35:05 -0500

    Homepage Content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <Header msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>READY TO WORK WITH MIAMI'S BEST?</h1>
+    <p>Miami Marketing Company is here to help you and your online business! See more of our services below!</p>
+    <h2>Miami Marketing Company- Miamis Leading Website & Marketing Firm</h2>
+    <p>Powerful Miami Website Deisgn and Development Serices. Miami's Leading Digital Marketing Firm.</p>
   </div>
 </template>
 
