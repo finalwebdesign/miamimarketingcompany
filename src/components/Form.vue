7509033513603aba6260f289005882e308d1bfc4
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-09-05 12:04:51 -0400

    Contact Form

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form">
+      <img alt="Miami Marketing Company Logo" src="../assets/logo.png" />
+    <h2>Contact Miami Marketing Company</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class ContactForm extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="contact">
-    <Header/>
-    <img src="../assets/contact-miami-marketing-company.jpg" title="Contact Marketing Company Services" alt="contact Miami Marketing Company Services">
- <h1>Contact Miami Marketing Company</h1>
-<p>Visit us at our downtown Brickell location:</p>
-<ul>
-  <li>Office: (888) 674-7779</li>
-  <li>801 Brickell Ave, Suite 942</li>
-  <li>Miami, FL 33131</li>
-  <li>Info@MiamiMarketingCompany.com</li>
-  <li>Miami Marketing Company, Inc.</li>
-</ul>
-    <Footer/>
+    <Header />
+    <img
+      src="../assets/contact-miami-marketing-company.jpg"
+      title="Contact Marketing Company Services"
+      alt="contact Miami Marketing Company Services"
+    />
+    <h1>Contact Miami Marketing Company</h1>
+    <p>Visit us at our downtown Brickell location:</p>
+    <ul>
+      <li>Office: (888) 674-7779</li>
+      <li>801 Brickell Ave, Suite 942</li>
+      <li>Miami, FL 33131</li>
+      <li>Info@MiamiMarketingCompany.com</li>
+      <li>Miami Marketing Company, Inc.</li>
+    </ul>
+    <Form />
+
+    <Footer />
   </div>
 </template>
 
@@ -19,29 +25,18 @@
 import { Options, Vue } from "vue-class-component";
 import Header from "@/components/Header.vue"; // @ is an alias to /src
 import Footer from "@/components/Footer.vue"; // @ is an alias to /src
+import Form from "@/components/Form.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     Header,
     Footer,
+    Form,
   },
 })
 export default class Home extends Vue {}
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
