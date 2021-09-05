4bea9f1eb9f0393b3e518b7c5a1f176e48d26989
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-09-05 12:17:04 -0400

    Contact Form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="form">
-      <img alt="Miami Marketing Company Logo" src="../assets/logo.png" />
     <h2>Contact Miami Marketing Company</h2>
+      <div class="name">
+        <label class="label" for="name">Name</label>
+        <input type="text" name="name" id="name" required="" v-model="name">
+      </div>
+      <div class="email">
+        <label class="label" for="email">Email</label>
+        <input type="text" name="email" id="email" required="" v-model="name">
+      </div>
+    
   </div>
 </template>
 
