2a421c995d97cd25722d7774d1f017208c8c6445
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-09-06 19:33:19 -0600

    Social Media Icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,13 @@
   <div class="footer">
       <img alt="Miami Marketing Company Logo" src="../assets/logo.png" />
     <h1>{{ msg }}</h1>
+    <v-btn
+    elevation="0"
+    target="_blank"
+    href="https://facebook.com/miamimarketingcompany"
+    >
+    <v-icon size="x-large">mdi-facebook</v-icon>
+    </v-btn>
     <p>Designed and Developed by <a href="https://finalwebdesign.com" target ="_blank" title="Final Web Design">Final Web Design</a>, Inc.<br>
       All Rights Reserved <strong>© Copyright 2021</strong></p>
   </div>
